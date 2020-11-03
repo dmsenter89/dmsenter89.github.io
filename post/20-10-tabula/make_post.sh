1069455c7b356bb90cdb9f16d81bedcbb477c73d
Michael Senter <dmsenter89@gmail.com>
2020-11-03 12:02:10 -0500

    fix typo in teacher pay post

--- post/20-10-tabula/make_post.sh
+++ post/20-10-tabula/make_post.sh
@@ -2,6 +2,6 @@
 
 jupyter nbconvert tabula_analysis.ipynb --to markdown --NBConverterApp.output_files_dir=.
 
-#cat tabula_analysis.md | tee -a index.md
+cat tabula_analysis.md | tee -a index.md
 
-#rm tabula_analysis.md
+rm tabula_analysis.md
